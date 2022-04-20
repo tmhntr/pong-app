93d755325ed107a7a95c6723b0f2cc3e1360b190
Tim Hunter <thunte27@uwo.ca>
2022-04-20 14:51:18 -0400

    :lipstick: UI: add responsive size for pong canvas

--- src/Pong.tsx
+++ src/Pong.tsx
@@ -1,11 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ClientGame } from "./utils/game";
 
 const Pong: React.FC<{ game: ClientGame }> = ({ game }) => {
-  const GAME_WIDTH = 800;
-  const GAME_HEIGHT = 600;
+  // let GAME_WIDTH = 800;
+  // const GAME_HEIGHT = 600;
   let ctx: CanvasRenderingContext2D | null = null;
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const calculateGameWidth = (width: number) => {
+    return width > 1000 ? 800 : width * 0.8;
+  };
+  const [width, setWidth] = useState<number>(
+    calculateGameWidth(window.innerWidth)
+  );
+
+  function handleWindowSizeChange() {
+    setWidth(calculateGameWidth(window.innerWidth));
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
 
   // const gid = useMemo(() => game.gid, [game]);
 
@@ -31,8 +48,8 @@ const Pong: React.FC<{ game: ClientGame }> = ({ game }) => {
       <canvas
         id="canvas"
         ref={canvasRef}
-        height={GAME_HEIGHT}
-        width={GAME_WIDTH}
+        width={width}
+        height={width * (3 / 4)}
       />
     </div>
   );
