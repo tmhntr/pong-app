7b60e77a18124d19bc068220c63da2e7d57a0886
Tim Hunter <thunte27@uwo.ca>
2022-04-21 00:57:53 -0400

    :bug: Fix: added persistent game with useGame

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Pong from "./Pong";
 import { ClientGame } from "./utils/game";
 import WaitingPage from "./WaitingPage";
 
-const game = new ClientGame();
+let game: ClientGame;
+const useGame = () => {
+  if (!game) {
+    game = new ClientGame();
+  }
+  return game;
+};
 
 function App() {
   const [page, setPage] = useState<"waiting" | "pong">("waiting");
+  const game = useGame();
   return (
     <div className="App">
       <header className="App-header"></header>
